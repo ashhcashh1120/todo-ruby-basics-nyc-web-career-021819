52461bbad7e666a11475fa78d9906803aba0dc3b
Ashley Archibald <aarchibald20@gmail.com>
2019-02-04 05:13:35 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,23 +3,23 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-
+  value = "Ashley"
 end
 
-def argue
-
+def argue(phase)
+  phase = "What are you talking about"
 end
 
-def greeting
+def greeting()
 
 end
 
 def return_a_value
-
+  phrase = "nice"
 end
 
 def last_evaluated_value
-
+  phrase = "expert"
 end
 
 def pizza_party
